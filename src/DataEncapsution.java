b933c06fc67b61cf27f652636e757fe616a2e655
Amandeep Kaur <amanguru@Amandeeps-MacBook-Air.local>
2021-04-10 18:58:35 -0400

    Initial commit

---
+++ src/DataEncapsution.java
@@ -0,0 +1,12 @@
+
+
+public class DataEncapsution {
+
+    public static void main(String[] a)
+    {
+        FullEncapsulation fullEncapsulation= new FullEncapsulation();
+        fullEncapsulation.setData(20,80/2);
+        System.out.println("Sum is = "+fullEncapsulation.getData());
+
+    }
+}
---
+++ src/FullEncapsulation.java
@@ -0,0 +1,13 @@
+
+public class FullEncapsulation {
+    private int a=20,b=40;
+
+    public int getData() {
+        return a+b;
+    }
+
+    public void setData(int b,int a) {
+        this.b = b;
+        this.a=a;
+    }
+}
